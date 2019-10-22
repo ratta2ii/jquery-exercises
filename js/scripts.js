a7e05473a3f2a32b90d33950c50ab6ff2a2f835b
Adilet Momunaliev and Trevor Mackin <trevor@>
2019-10-22 11:59:10 -0700

    Adding JQuery methods such as: fade, slide, toggle

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,46 @@ $(document).ready(function () {
   });
 
 
+  $('.fade-out-button').click(function () {
+    $('#cricket').fadeOut(500);
+  });
+
+  $('.fade-in-button').click(function () {
+    $('#cricket').fadeIn(500);
+  });
+
+  $('.fade-toggle-button').click(function () {
+    $('#cricket').fadeToggle(500);
+  });
+
+
+  /*
+
+  */
+  $('.slide-down-button').click(function () {
+    $('#veggies').slideDown(500);
+  });
+
+  $('.slide-up-button').click(function () {
+    $('#veggies').slideUp(500);
+  });
+
+  $('.slide-toggle-button').click(function () {
+    $('#veggies').slideToggle(500);
+  });
+
+  $(".yellow-color").click(function(){
+    $("body").addClass("body-yellow-class");
+  });
+
+  $(".green-color").click(function(){
+    $("body").addClass("body-green-class");
+  });
+
+  $(".red-color").click(function(){
+    $("body").addClass("body-red-class");
+  });
+
 
 
 });
