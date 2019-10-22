53030a6409c076aa7c185a26371c2cda1f6f0641
Adilet Momunaliev and Trevor Mackin <trevor@>
2019-10-22 10:58:38 -0700

    Beginning of jquery exercises.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+  $(".text-click").click(function() {
+    $("#flower").toggle();
+    $('.text-click').toggle();
+    $('.text-click-off').toggle();
+  });
+
+  $(".text-click-off").click(function() {
+    $("#flower").toggle();
+    $('.text-click').toggle();
+    $('.text-click-off').toggle();
+  });
+
+
+
+
+});
